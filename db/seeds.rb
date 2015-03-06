58e63de5538190d7dc8e9d6f425b63bbe27ae9e2
conradmyla <childelevate.ie>
2015-03-06 17:19:16 +0000

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "conrad1", email: "con1@gmail.com", password: "admin123", password_confirmation: "admin123")
+User.create(username: "conrad2", email: "con2@gmail.com", password: "admin123", password_confirmation: "admin123")
+User.create(username: "conrad3", email: "con3@gmail.com", password: "admin123", password_confirmation: "admin123")
+User.create(username: "conrad4", email: "con4@gmail.com", password: "admin123", password_confirmation: "admin123")
+User.create(username: "conrad5", email: "con5@gmail.com", password: "admin123", password_confirmation: "admin123")
+User.create(username: "conrad6", email: "con6@gmail.com", password: "admin123", password_confirmation: "admin123")
+
+p "Test Users created"
