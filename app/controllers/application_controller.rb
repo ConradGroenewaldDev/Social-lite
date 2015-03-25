8d79f5779b1fc82300126b42a1d3cec24ca06711
conradmyla <childelevate.ie>
2015-03-25 00:26:51 +0000

    added finishing touches

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+	#make helper for request_count method
+	helper_method :request_count
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  #create method for requests counts/ use method partial from users_controller
+  def request_count
+  	current_user.pending_friend_requests_from.map(&:user).size
+  	
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
+  	#add condition so when logged in not to direct to landing page when clicking menu title
+  	if current_user
+  		redirect_to activities_path
+  	end
   end
 end
