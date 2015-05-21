77c116d48809a1cdf5e27e26d897871edb11f164
conradmyla <childelevate.ie>
2015-05-21 21:46:20 +0100

    javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,12 +15,3 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
-$(document).ready(function () {
- 
-window.setTimeout(function() {
-    $(".alert").fadeTo(1500, 0).slideUp(500, function(){
-        $(this).remove(); 
-    });
-}, 5000);
- 
-});
